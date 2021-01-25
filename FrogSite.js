9f60361ce67bcb40363ebf26596f174e42dc5387
MarkTkachenko <mark.morkovkin@gmail.com>
2021-01-25 19:37:30 +0200

    initial

---
+++ FrogSite.js
@@ -0,0 +1,63 @@
+let mainDiv = document.getElementById('mainDiv')
+let CenterDiv = document.createElement('div')
+let divForLogo = document.createElement('div')
+let divForText = document.createElement('div')
+let rightDiv = document.createElement('div')
+let leftPanel = document.createElement('div')
+let bottomDiv = document.createElement('div')
+let menuClass = document.createElement('div')
+
+leftPanel.className = 'leftPanel'
+CenterDiv.className = 'centerDiv'
+divForLogo.className = 'divLogo'
+divForText.className = 'textDiv'
+rightDiv.className = 'rightDiv'
+
+divForLogo.innerHTML = '<img src="DONE3.png" alt="PORNFROG" style="margin-top: -300px; margin-bottom: 100px; margin-left: 100px">'
+rightDiv.innerHTML = '<h1>тупая реклама 1xbet а так же Reid Shadow Legens и ставок на спорт и твою жизнь))</h1>'
+
+divForText.innerHTML = '<div class="stand"><img src="1.jfif" alt="обычное"><a href="https://rt.pornhub.com/"><h3 style="margin-top: -5px; margin-left: 70px">обычное</h3></a></div>'
+    + '<div class="stand5"><img src="2.jfif" alt="тройничёк"><a href="https://rt.pornhub.com/video/search?search=%D0%B6%D0%BC%D0%B6"><h3 style="margin-top: -5px; margin-left: 70px">тройничёк</h3></a></div>'
+    + '<div class="stand2"><img src="3.jfif" alt="молоденькие"><a href="https://rt.pornhub.com/categories/teen"><h3 style="margin-top: -5px; margin-left: 70px">молоденькие</h3></a></div>'
+    + '<div class="stand3"><img src="4.jfif" alt="уроки"><a href="https://rt.pornhub.com/video/search?search=%D0%BA%D0%B0%D0%BC%D0%B0%D1%81%D1%83%D1%82%D1%80%D0%B0"><h3 style="margin-top: -5px; margin-left: 80px">Камасутра</h3></a></div>'
+    + '<div class="stand4"><img src="5.jfif" alt="уроки"><a href="https://rt.pornhub.com/categories/hentai"><h3 style="margin-top: -5px; margin-left: 80px">Хентай</h3></a></div>'
+    + '<div class="stand6"><img src="6.jpg" alt="уроки" style="width: 275px"><a href="https://rt.pornhub.com/categories/hentai"><h3 style="margin-top: -5px; margin-left: 80px">гей порно</h3></a></div>'
+
+
+leftPanel.innerHTML = '<div> <img src="жабки.png" alt="logo" style="width: 300px;"></div>' + '<div><h1 class="white">ЖАБКИ</h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/">обычное</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/video/search?search=%D0%B6%D0%BC%D0%B6">тройничёк</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/categories/teen">молоденькие</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/video/search?search=%D0%BA%D0%B0%D0%BC%D0%B0%D1%81%D1%83%D1%82%D1%80%D0%B0">Камасутра</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/video?c=27">Лезбийское</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/gayporn">Гей порно</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/video/search?search=%D0%BE%D1%80%D0%B3%D0%B8%D1%8F">Оргия</a></h1></div>'
+    + '<div class="wh"><h1><a href="https://rt.pornhub.com/categories/hentai">Хентай</a></h1></div>'
+    + '<div>создатели:</div>'
+    + '<div>дизайн: masseru, HeronDied</div>'
+    + '<div>програмисты: HeronDied</div>'
+    + '<div>discord: HeronDied#4660</div>'
+
+
+mainDiv.appendChild(leftPanel)
+mainDiv.appendChild(bottomDiv)
+leftPanel.appendChild(CenterDiv)
+leftPanel.appendChild(rightDiv)
+CenterDiv.appendChild(divForLogo)
+CenterDiv.appendChild(divForText)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
